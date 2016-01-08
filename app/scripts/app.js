7a3efaf395dd525418507a6d60677726bf1e4546
jackniland <jack.niland@uk.ibm.com>
2016-01-08 17:21:06 +0000

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rockPaperScissorsApp
+ * @description
+ * # rockPaperScissorsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rockPaperScissorsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rockPaperScissorsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rockPaperScissorsApp
+ */
+angular.module('rockPaperScissorsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
