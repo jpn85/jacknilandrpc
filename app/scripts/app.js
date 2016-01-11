e17d985276702dcb08bb3f400545cc0bb64ab814
jackniland <jack.niland@uk.ibm.com>
2016-01-11 12:18:02 +0000

    deletions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,4 +26,6 @@ angular.module('rockPaperScissorsApp', [
             .otherwise({
                 redirectTo: '/'
             });
+
+
     });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,13 @@
  * Controller of the rockPaperScissorsApp
  */
 angular.module('rockPaperScissorsApp')
-    .controller('MainCtrl', function () {
+    .controller('MainCtrl', function ($scope) {
         this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+        // create a message to display in our view
+        $scope.message = 'test test';
+
     });
